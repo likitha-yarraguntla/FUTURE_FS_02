5809c6f457547b5bba18af11b8be997715bc24fc
Likitha Yarraguntla <likithayarraguntla.2006@gmail.com>
2026-06-25 09:09:42 +0530

    Added vercel routing

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,7 +17,7 @@ setFormData({
 [e.target.name]: e.target.value,
 });
 };
-
+const API_URL = import.meta.env.VITE_API_URL;
 const handleSubmit = async (e) => {
 e.preventDefault();
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -25,7 +25,7 @@ e.preventDefault();
 
 try {
   await axios.post(
-    "https://leadflow-crm-f796.onrender.comcd/api/auth/register",
+    "https://leadflow-crm-f796.onrender.com/api/auth/register",
     formData
   );
 
